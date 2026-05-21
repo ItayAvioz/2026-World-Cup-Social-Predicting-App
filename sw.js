07ab3d0386f88b07a0fbf02ad5b3a8b57fd3c06c
yonatanam <yonatanam@outlook.com>
2026-05-21 16:34:37 +0300

    deploy: SW v21 + app-DWUlVBmy.js (+0 assets)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const SW_VERSION = '20'
+const SW_VERSION = '21'
 const ICON = 'https://itayavioz.github.io/2026-World-Cup-Social-Predicting-App/icon-notif.png'
 
 self.addEventListener('install', (event) => {
