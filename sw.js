7821dadb875daf971ffe5adec48e91216a673401
yonatanam <yonatanam@outlook.com>
2026-05-24 20:52:25 +0300

    chore: bump SW_VERSION to 28

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const SW_VERSION = '27'
+const SW_VERSION = '28'
 const ICON = 'https://itayavioz.github.io/2026-World-Cup-Social-Predicting-App/icon-notif.png'
 
 self.addEventListener('install', (event) => {
